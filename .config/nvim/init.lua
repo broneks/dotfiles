e5ea5e7641aeff764b974862c0e9863adf94fdbf
Bronek Szulc <bszulc@coursera.org>
2022-03-09 16:19:48 -0500

    combining node version line with coc config import

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,13 @@
--- TODO: remove once web repo bumps node version
-vim.cmd [[let $PATH = '/Users/bszulc/.nvm/versions/node/v14.18.1/bin:' . $PATH]]
-
 require 'globals'
 require 'settings'
 require 'plugins'
-require 'plugin_configs'
 
 -- TODO: convert to lua or switch to native LSP
-vim.cmd [[source ~/.config/nvim/coc.vim]]
+vim.cmd [[
+  let $PATH = '/Users/bszulc/.nvm/versions/node/v14.18.1/bin:' . $PATH
+  source ~/.config/nvim/coc.vim
+]]
 
+require 'plugin_configs'
 require 'mappings'
 require 'autocmds'
