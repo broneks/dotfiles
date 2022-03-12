02e0de14463bfb1af663a5d0e82ce1c275e84022
Bronek Szulc <bszulc@coursera.org>
2022-03-11 22:58:44 -0500

    simplified clone script

--- .dot/setup.sh
+++ .dot/setup.sh
@@ -3,14 +3,19 @@
 # bare repo
 
 # git clone --bare git@github.com:broneks/dotfiles.git $HOME/.cfg
-# config config --local status.showUntrackedFiles no
+# function config {
+#    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+# }
+# mkdir -p .config-backup
 # config checkout
-#
-# backup if checkout fails
-#
-# mkdir -p $HOME/.config-backup && \
-# config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
-# xargs -I{} mv {} $HOME/.config-backup/{}
+# if [ $? = 0 ]; then
+#   echo "Checked out config.";
+#   else
+#     echo "Backing up pre-existing dot files.";
+#     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+# fi;
+# config checkout
+# config config status.showUntrackedFiles no
 
 # zsh
 
