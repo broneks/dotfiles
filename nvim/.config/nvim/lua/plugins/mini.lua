5913dd17bd0fddf94303adaff81f1f5641b8f5c9
Bronek Szulc <bronek@fastmail.fm>
2023-02-04 15:57:43 -0500

    mini fix

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,13 +2,22 @@ return {
   {
     'echasnovski/mini.comment',
     version = '*',
+    config = function()
+      require('mini.comment').setup()
+    end,
   },
   {
     'echasnovski/mini.pairs',
     version = '*',
+    config = function()
+      require('mini.pairs').setup()
+    end,
   },
   {
     'echasnovski/mini.surround',
     version = '*',
+    config = function()
+      require('mini.surround').setup()
+    end,
   },
 }
