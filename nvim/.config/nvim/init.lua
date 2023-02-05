78c59d5cea8ac046158bca1f41c439ebe8196847
Bronek Szulc <bronek@fastmail.fm>
2023-02-04 20:27:53 -0500

    cleanup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,36 +1,4 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-require('lazy').setup('plugins', {
-  install = {
-    colorscheme = {
-      'duskfox',
-      'tokyonight',
-      'habamaz',
-    },
-  },
-  checker = {
-    enabled = true,
-    frequency = 43200, -- twice a day
-  },
-  ui = {
-    border = 'rounded',
-  },
-})
-
+require 'manager'
 require 'globals'
 require 'settings'
 require 'mappings'
