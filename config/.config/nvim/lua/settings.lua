dca0f39d35ea1c3493d9d64343036b770213594a
Bronek Szulc <bronek@fastmail.fm>
2023-07-11 16:45:37 -0700

    updating gutter signs

--- config/.config/nvim/lua/settings.lua
+++ config/.config/nvim/lua/settings.lua
@@ -91,7 +91,7 @@ g.loaded_perl_provider = 0
 d.config({ virtual_text = false })
 
 -- gutter signs
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
 for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
