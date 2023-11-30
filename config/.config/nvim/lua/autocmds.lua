40655b7884572f6827ac7512494ad30ae4954671
Bronek Szulc <bronek@fastmail.fm>
2023-11-30 16:23:22 -0500

    updating the js/ts format autocmd

--- config/.config/nvim/lua/autocmds.lua
+++ config/.config/nvim/lua/autocmds.lua
@@ -1,9 +1,15 @@
 local cmd = vim.cmd
 
-cmd 'au ColorScheme * hi Normal ctermbg=none guibg=none'
+cmd 'autocmd ColorScheme * hi Normal ctermbg=none guibg=none'
 
 -- highlight yank for a brief second for visual feedback
 cmd 'autocmd! TextYankPost * lua vim.highlight.on_yank { on_visual = false }'
 
 -- automagically format on save
-cmd 'autocmd BufWritePre *.js,*.ts Neoformat'
+cmd [[
+  augroup fmt
+    autocmd!
+    autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx undojoin | EslintFixAll
+    autocmd BufWritePre *.js,*.jsx,*.ts,*.tsx undojoin | Neoformat
+  augroup END
+]]
