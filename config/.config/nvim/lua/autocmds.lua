3716ae63dbcce94e44f928ddef20dad15615a59a
Bronek Szulc <bronek@fastmail.fm>
2024-11-01 19:40:46 -0400

    Add go plugins

--- config/.config/nvim/lua/autocmds.lua
+++ config/.config/nvim/lua/autocmds.lua
@@ -18,3 +18,12 @@ cmd [[
     autocmd! BufWritePre *.js,*.jsx,*.ts,*.tsx EslintFixAll
   augroup END
 ]]
+
+local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
