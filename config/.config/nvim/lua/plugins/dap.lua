5a38998b15ed29fbf60f096b796a568f22f3c1a4
Bronek Szulc <bronislaw.szulc@workleap.com>
2025-11-21 16:59:32 -0500

    cut out the middleman

--- config/.config/nvim/lua/plugins/dap.lua
+++ config/.config/nvim/lua/plugins/dap.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'mxsdev/nvim-dap-vscode-js',
+    'mfussenegger/nvim-dap',
     dependencies = {
       {
         'microsoft/vscode-js-debug',
@@ -10,26 +10,14 @@ return {
         'Joakker/lua-json5',
         build = './install.sh',
       },
-      'mfussenegger/nvim-dap',
       'rcarriga/nvim-dap-ui',
       'nvim-neotest/nvim-nio',
     },
     config = function()
       local dap = require('dap')
       local dap_ui = require('dapui')
-      local dap_vscode = require('dap-vscode-js')
 
       local debugger_path = vim.fn.resolve(vim.fn.stdpath('data') .. '/lazy/vscode-js-debug')
-
-      dap_vscode.setup({
-        node_path = 'node', -- Path of node executable. Defaults to $NODE_PATH, and then 'node'
-        debugger_path = debugger_path,
-        -- debugger_cmd = { 'js-debug-adapter' }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { 'pwa-node', --[['pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost']] }, -- which adapters to register in nvim-dap
-        -- log_file_path = '(stdpath cache)/dap_vscode_js.log' -- Path for file logging
-        -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-        -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-      })
 
       dap.adapters['pwa-node'] = {
         type = 'server',
