1397fe2b6407511410bd0c1fa60fd4d8e4192204
Bronek Szulc <bronislaw.szulc@workleap.com>
2025-11-21 10:59:58 -0500

    use nvim-dap-vscode-js

--- config/.config/nvim/lua/plugins/dap.lua
+++ config/.config/nvim/lua/plugins/dap.lua
@@ -1,66 +1,19 @@
 return {
   {
-    'mfussenegger/nvim-dap',
+    'mxsdev/nvim-dap-vscode-js',
     dependencies = {
+      {
+        'microsoft/vscode-js-debug',
+        opt = true,
+        run = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+      },
+      'mfussenegger/nvim-dap',
+      'rcarriga/nvim-dap-ui',
       'nvim-neotest/nvim-nio',
     },
     config = function()
-      local dap = require('dap')
-
-      dap.adapters.node2 = {
-        type = 'executable',
-        command = 'node',
-        args = {os.getenv('HOME') .. '/debugger/microsoft/vscode-node-debug2/out/src/nodeDebug.js'},
-      }
-
-      dap.configurations.javascript = {
-        {
-          name = 'Launch',
-          type = 'node2',
-          request = 'launch',
-          program = '${file}',
-          cwd = vim.fn.getcwd(),
-          sourceMaps = true,
-          protocol = 'inspector',
-          console = 'integratedTerminal',
-        },
-        {
-          -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-          name = 'Attach to process',
-          type = 'node2',
-          request = 'attach',
-          processId = require'dap.utils'.pick_process,
-        },
-      }
-
-      -- dap.adapters.chrome = {
-      --   type = 'executable',
-      --   command = 'node',
-      --   args = { os.getenv('HOME') .. '/debugger/microsoft/vscode-chrome-debug/out/src/chromeDebug.js' }
-      -- }
-
-      -- dap.configurations.javascriptreact = {
-      --   {
-      --     name = 'Debug (Attach) - Remote',
-      --     type = 'chrome',
-      --     request = 'attach',
-      --     program = '${file}',
-      --     cwd = vim.fn.getcwd(),
-      --     sourceMaps = true,
-      --     trace = true,
-      --     protocol = 'inspector',
-      --     port = 9222,
-      --     webRoot = '${workspaceFolder}'
-      --   }
-      -- }
-    end,
-  },
-  {
-    'rcarriga/nvim-dap-ui',
-    dependencies = {'mfussenegger/nvim-dap'},
-    config = function()
-      local dap = require('dap')
-      local dapui = require('dapui')
+       local dap = require('dap')
+       local dapui = require('dapui')
 
       dapui.setup({
         icons = { expanded = '', collapsed = '', current_frame = '' },
@@ -82,8 +35,7 @@ return {
           -- }
         },
         -- Expand lines larger than the window
-        -- Requires >= 0.7
-        expand_lines = vim.fn.has('nvim-0.7') == 1,
+        expand_lines = true,
         -- Layouts define sections of the screen to place windows.
         -- The position can be 'left', 'right', 'top' or 'bottom'.
         -- The size specifies the height/width depending on position. It can be an Int
@@ -141,17 +93,71 @@ return {
         }
       })
 
-      dap.listeners.after.event_initialized['dapui_config'] = function()
+       dap.listeners.before.attach.dapui_config = function()
         dapui.open()
-      end
+       end
 
-      dap.listeners.before.event_terminated['dapui_config'] = function()
-        dapui.close()
-      end
+       dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+       end
 
-      dap.listeners.before.event_exited['dapui_config'] = function()
+       dap.listeners.before.event_terminated.dapui_config = function()
         dapui.close()
+       end
+
+       dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+       end
+
+      require('dap-vscode-js').setup({
+        -- node_path = 'node', -- Path of node executable. Defaults to $NODE_PATH, and then 'node'
+        -- debugger_path = '(runtimedir)/site/pack/packer/opt/vscode-js-debug', -- Path to vscode-js-debug installation.
+        -- debugger_cmd = { 'js-debug-adapter' }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+        adapters = { 'pwa-node', 'pwa-chrome', --[['pwa-msedge', 'node-terminal', 'pwa-extensionHost']] }, -- which adapters to register in nvim-dap
+        -- log_file_path = '(stdpath cache)/dap_vscode_js.log' -- Path for file logging
+        -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+        -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+      })
+
+      for _, language in ipairs({ 'typescript', 'javascript' }) do
+        require('dap').configurations[language] = {
+          {
+            -- Node.js
+            {
+              type = 'pwa-node',
+              request = 'launch',
+              name = 'Launch file',
+              program = '${file}',
+              cwd = '${workspaceFolder}',
+            },
+            {
+              type = 'pwa-node',
+              request = 'attach',
+              name = 'Attach',
+              processId = require('dap.utils').pick_process,
+              cwd = '${workspaceFolder}',
+            },
+            -- Jest
+            {
+              {
+                type = 'pwa-node',
+                request = 'launch',
+                name = 'Debug Jest Tests',
+                -- trace = true, -- include debugger info
+                runtimeExecutable = 'node',
+                runtimeArgs = {
+                  './node_modules/jest/bin/jest.js',
+                  '--runInBand',
+                },
+                rootPath = '${workspaceFolder}',
+                cwd = '${workspaceFolder}',
+                console = 'integratedTerminal',
+                internalConsoleOptions = 'neverOpen',
+              }
+            }
+          }
+        }
       end
     end,
-  },
+  }
 }
